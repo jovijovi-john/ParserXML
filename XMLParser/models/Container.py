38b655f8da68f62676b9fe6510703035f7e486f5
John <jovijovi2019@hotmail.com>
2022-11-20 23:46:23 -0300

    Bugfix: Atributos novos sobreescrevendo os antigo

--- XMLParser/models/Container.py
+++ XMLParser/models/Container.py
@@ -1,14 +1,26 @@
-class Container:
-  
-  def __init__(self, Container):
+class Container: 
+  def __init__(self, Container): 
 
     #self.name  >>>  guarda o tipo do container
-    #self.item  >>>  guarda um item
+    #self.item  >>>  guarda os itens do container
     #self.status  >>>  guarda o status do container
     #self.trigger  >>>  guarda um trigger
-    
+
     for att in Container:
       tag = att.tag
-      setattr(self, tag, att)
+
+      try: 
+        # se já existe valor no atributo
+        value = getattr(self, tag)
+
+        # se não é uma lista
+        if (not isinstance(value, list)):
+          setattr(self, tag, [value, att])
+        else:
+          value.append(att)
+          
+      except(AttributeError):
+        # não existe valor no atributo
+        setattr(self, tag, att)
 
     
